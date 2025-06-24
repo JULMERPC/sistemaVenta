f1c56697eaec6f47133934eef915a91cbb53107a
Julmer Puma <julmerpumacondori@gmail.com>
2025-06-23 20:35:14 -0500

    arreglado2

--- dad-front/src/app/core/interceptors/token-interceptor.ts
+++ dad-front/src/app/core/interceptors/token-interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   //const token = localStorage.getItem('access_token');
-  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJub2UiLCJpZCI6MSwiaWF0IjoxNzUwMDk0MjcwLCJleHAiOjE3NTAwOTc4NzB9.8_kOzCc_G6_zbPb6KRlZSEFmouTfm8VoJx2GMcgXVdE"
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWxtZWFyIiwiaWQiOjEsImlhdCI6MTc1MDcyODU2OSwiZXhwIjoxNzUwNzMyMTY5fQ.asV957o5ELg4-F5ikFA0SPq6tGEZLMK_CwdEz3zmaPI"
 
   if (token) {
     const authReq = req.clone({
--- dad-frontend/src/app/app.ts
+++ dad-frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ApiService} from './core/services/api.service';
 
